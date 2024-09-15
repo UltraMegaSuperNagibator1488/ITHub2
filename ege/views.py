3f291ab980330d33396872299e7db8bb3ed2c3ab
guest <guest@robbit.local>
2024-09-15 14:50:26 +0300

    add_table

--- ege/views.py
+++ ege/views.py
@@ -2,16 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+class Task:
+
+    def __init__(self, id: int, ege_num: int, desc: str, ege_data: int | None):
+        self.id = id
+        self.ege_num = ege_num
+        self.desc = desc
+        self.ege_data = ege_data
+
+task1 = Task(1, 27, 'aaaa', 2020)
+task2 = Task(2, 25, 'bbbb', None)
+
+
 def index(request):
     return HttpResponse('<h1>Ege main page</h1><a href="tasks/">Tasks list</a><br><a href="preparation/">Preparation materials</a>')
 
 
-def tasks_main_page(request):
-    return HttpResponse('<h1>List of tasks</h1>')
-
-
-def tasks(request, task_id):
-    return HttpResponse(f'<h1>Task №{task_id}</h1>')
+def tasks(request):
+    return render(request, 'task/index.html', context={'tasks': [task1, task2]})
 
 
 def preparation_materials(request):
